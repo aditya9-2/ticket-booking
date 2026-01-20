5ac866149ed9487475222a20f237af3c10b96c19
Aditya Basak <adityabasak7031@gmail.com>
2026-01-21 00:23:27 +0530

    add router to dee the booked events

--- src/routes/bookingRouter.ts
+++ src/routes/bookingRouter.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { createBookingController } from "../controller/bookingController.js";
+import { createBookingController, getYourBookingController } from "../controller/bookingController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post('/create-booking',authMiddleware, createBookingController);
+router.get('/your-booking', authMiddleware, getYourBookingController);
 
 export default router;
